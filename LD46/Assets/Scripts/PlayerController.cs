fa9d35e5e2302c35b9c75f7ae47d32414a603aef
Robin <robin.flavigny@free.fr>
2020-04-19 13:11:16 +0200

    Mise en place de la version 0.1 avec obstacles et murs

---
+++ LD46/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public string inputFront;
+    public string inputBack;
+    public string inputLeft;
+    public string inputRight;
+
+    public float speed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetKey(inputFront))
+        {
+            transform.Translate(0, speed * Time.deltaTime, 0);
+        }
+
+        if (Input.GetKey(inputBack))
+        {
+            transform.Translate(0, -speed * Time.deltaTime, 0);
+        }
+
+        if (Input.GetKey(inputLeft))
+        {
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(inputRight))
+        {
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+    }
+}
